144d1704de87ec51b7a3806ee32d2173a052a08b
liqiang <liqiang@300.cn>
2021-01-25 15:54:28 +0800

    更换项目框架

--- src/main.ts
+++
@@ -1,5 +0,0 @@
-import { createApp } from 'vue'
-import App from './layout/app.vue'
-import router from "./router/index"
-import elementPlus from './plugins/ui/element-plus'
-createApp(App).use(router).use(elementPlus).mount('#app')
