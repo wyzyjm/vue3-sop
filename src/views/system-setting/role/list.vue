07869e6ffe5fd1e25d85304a2d50cddd111ab934
liqiang <liqiang@300.cn>
2021-02-02 16:52:47 +0800

    修改写法

--- src/views/system-setting/role/list.vue
+++ src/views/system-setting/role/list.vue
@@ -1,11 +1,11 @@
 <template>
   <div>
     <s-dialog width="500px" title="新增角色组" @close="$store.commit('dialog/close',{_uid:'add-role-group'})" :component="require('./dialog/add-role-group')" uid="add-role-group" />
-    <s-dialog width="500px" title="新增角色" @close="$store.commit('dialog/close',{_uid:'add-role'})" :component="require('./dialog/add-role')" uid="add-role" />
+    <s-dialog width="500px" :title="role.title" @close="role.close" :component="require('./dialog/add-role')" uid="add-role" />
     <s-simple-table :data="table.data" :cols="table.cols">
       <div slot="top" class="mb20">
         <s-button type="primary" @click="$store.commit('dialog/open',{_uid:'add-role-group'})">新增角色组</s-button>
-        <s-button type="primary" @click="$store.commit('dialog/open',{_uid:'add-role'})">新增角色</s-button>
+        <s-button type="primary" @click="role.add">新增角色</s-button>
         <s-button type="primary" @click="functionAuthorization">功能授权</s-button>
         <s-button type="primary" @click="productionOrganizationAuthorization">生产组织授权</s-button>
         <s-button type="primary" @click="salesChannelsAuthorization">售卖渠道授权</s-button>
@@ -34,13 +34,33 @@ export default defineComponent({
     const productionOrganizationAuthorization = () => {}
     const salesChannelsAuthorization = () => {}
 
+    const role = reactive({
+      title: '新增角色',
+      add() {
+        role.title = '新增角色'
+        root.$store.commit('dialog/open', { _uid: 'add-role' })
+      },
+      edit(data) {
+        role.title = '编辑角色'
+        root.$store.commit('dialog/open', {
+          _uid: 'add-role',
+          isEdit: true,
+          data,
+        })
+      },
+      close() {
+        root.$store.commit('dialog/close', { _uid: 'add-role' })
+      },
+      setState(row) {
+        return setRoleState(row).then(({ msg }) => {
+          console.log(msg)
+          root.$store.commit('table/update')
+        })
+      },
+    })
+
     const table = reactive({
       data: getTableData,
-      // data: [
-      //   {
-      //     roleName: '测试',
-      //   },
-      // ],
       cols: [
         {
           showOverflowTooltip: true,
@@ -71,28 +91,11 @@ export default defineComponent({
           label: '操作项',
           prop: ({ row }) => {
             return [
-              <s-button
-                type="text"
-                onClick={() => {
-                  root.$store.commit('dialog/open', {
-                    _uid: 'add-role',
-                    isEdit: true,
-                    data: row,
-                  })
-                }}
-              >
+              <s-button type="text" onClick={() => role.edit(row)}>
                 编辑
               </s-button>,
-              <s-button
-                type="text"
-                onClick={() => {
-                  return setRoleState(row).then(({ msg }) => {
-                    console.log(msg)
-                    root.$store.commit('table/update')
-                  })
-                }}
-              >
-                停用
+              <s-button type="text" onClick={() => role.setState(row)}>
+                {row.state ? '启用' : '停用'}
               </s-button>,
             ]
           },
@@ -103,6 +106,7 @@ export default defineComponent({
     const options = useOptions()
 
     return {
+      role,
       table,
       options,
       functionAuthorization,
