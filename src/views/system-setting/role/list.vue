6a1bd3c2b3e8338e96b381c6a69c6cb26566d344
liqiang <liqiang@300.cn>
2021-02-03 15:30:18 +0800

    other

--- src/views/system-setting/role/list.vue
+++ src/views/system-setting/role/list.vue
@@ -3,14 +3,17 @@
 
     <s-dialog v-bind="roleDialog" @close="roleDialog.close" :component="require('./dialog/role')" />
     <s-dialog v-bind="roleGroupDialog" @close="roleGroupDialog.close" :component="require('./dialog/role-group')" />
+    <s-dialog v-bind="functionAuthorizationDialog" @close="functionAuthorizationDialog.close" :component="require('./dialog/function-authorization')" />
+    <s-dialog v-bind="productionOrganizationAuthorizationDialog" @close="productionOrganizationAuthorizationDialog.close" :component="require('./dialog/production-organization-authorization')" />
+    <s-dialog v-bind="salesChannelsAuthorizationDialog" @close="salesChannelsAuthorizationDialog.close" :component="require('./dialog/sales-channels-authorization')" />
 
     <s-simple-table :data="table.data" :cols="table.cols">
       <div slot="top" class="mb20">
         <s-button type="primary" @click="roleGroupDialog.openAdd">新增角色组</s-button>
         <s-button type="primary" @click="roleDialog.openAdd">新增角色</s-button>
-        <s-button type="primary" @click="functionAuthorization">功能授权</s-button>
-        <s-button type="primary" @click="productionOrganizationAuthorization">生产组织授权</s-button>
-        <s-button type="primary" @click="salesChannelsAuthorization">售卖渠道授权</s-button>
+        <s-button type="primary" @click="functionAuthorizationDialog.open">功能授权</s-button>
+        <s-button type="primary" @click="productionOrganizationAuthorizationDialog.open">生产组织授权</s-button>
+        <s-button type="primary" @click="salesChannelsAuthorizationDialog.open">售卖渠道授权</s-button>
       </div>
       <s-form slot="form" inline>
         <s-form-item label="角色名称" prop="roleName" />
@@ -78,9 +81,53 @@ export default defineComponent({
       },
     })
 
-    const functionAuthorization = () => {}
-    const productionOrganizationAuthorization = () => {}
-    const salesChannelsAuthorization = () => {}
+    const functionAuthorizationDialog = reactive({
+      uid: 'function-authorization',
+      title: '功能授权',
+      width: '500px',
+      open() {
+        root.$store.commit('dialog/open', {
+          _uid: functionAuthorizationDialog.uid,
+        })
+      },
+      close() {
+        root.$store.commit('dialog/close', {
+          _uid: functionAuthorizationDialog.uid,
+        })
+      },
+    })
+
+    const productionOrganizationAuthorizationDialog = reactive({
+      uid: 'production-organization-authorization',
+      title: '生产组织授权',
+      width: '500px',
+      open() {
+        root.$store.commit('dialog/open', {
+          _uid: productionOrganizationAuthorizationDialog.uid,
+        })
+      },
+      close() {
+        root.$store.commit('dialog/close', {
+          _uid: productionOrganizationAuthorizationDialog.uid,
+        })
+      },
+    })
+
+    const salesChannelsAuthorizationDialog = reactive({
+      uid: 'production-organization-authorization',
+      title: '生产组织授权',
+      width: '500px',
+      open() {
+        root.$store.commit('dialog/open', {
+          _uid: salesChannelsAuthorizationDialog.uid,
+        })
+      },
+      close() {
+        root.$store.commit('dialog/close', {
+          _uid: salesChannelsAuthorizationDialog.uid,
+        })
+      },
+    })
 
     const setState = (row) => {
       return setRoleState(row).then(({ msg }) => {
@@ -136,13 +183,13 @@ export default defineComponent({
     const options = useOptions()
 
     return {
+      functionAuthorizationDialog,
       roleGroupDialog,
       roleDialog,
       table,
       options,
-      functionAuthorization,
-      productionOrganizationAuthorization,
-      salesChannelsAuthorization,
+      productionOrganizationAuthorizationDialog,
+      salesChannelsAuthorizationDialog,
     }
   },
 })
