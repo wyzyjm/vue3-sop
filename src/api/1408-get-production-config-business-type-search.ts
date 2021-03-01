ec3e5ec4c9098bc1ef4de22638eb23fc8ec584dc
howie <tianhangyu@300.cn>
2021-03-01 14:41:18 +0800

    add

--- src/api/1408-get-production-config-business-type-search.ts
+++ src/api/1408-get-production-config-business-type-search.ts
@@ -1,11 +1,7 @@
 
 /**
  * 文档地址：http://10.12.52.224:3000/project/119/interface/api/1408
-<<<<<<< HEAD
- * 生成日期：Mon Mar 01 2021 11:01:22 GMT+0800 (GMT+08:00)
-=======
  * 生成日期：Mon Mar 01 2021 12:48:12 GMT+0800 (GMT+08:00)
->>>>>>> cab8698de2dcc885ae863e438a9297e97677c53e
  * 生成工具版本：ts-v0.0.2
  * 接口名称：搜索
  * 创建人：杨一点
@@ -33,10 +29,6 @@ export interface ReqQuery {
   pageSize?:string    
 }
 
-<<<<<<< HEAD
-=======
-
->>>>>>> cab8698de2dcc885ae863e438a9297e97677c53e
 interface _Params extends ReqQuery {}
 
 import request from '../plugins/axios/index.js'
@@ -46,8 +38,4 @@ export default (params: _Params)=> {
         method:'GET',
         params:params
     })
-<<<<<<< HEAD
 }
-=======
-}
->>>>>>> cab8698de2dcc885ae863e438a9297e97677c53e
