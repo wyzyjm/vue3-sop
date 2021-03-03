08ecefb36891df35035110f25fd4688f6ba4f63b
liqiang <liqiang@300.cn>
2021-03-03 10:46:03 +0800

    生产周期

---
+++ src/views/produce/product-period/dialog/set.vue
@@ -0,0 +1,73 @@
+<template>
+  <div>
+    <s-form :model="form" label-width="180px" @submit="save">
+      <s-form-item component="el-input-number" :precision="0" :min="0" label="基准生产周期（天）" prop="baseCycle" />
+      <s-form-item component="el-input-number" :precision="0" :min="0" label="同风格时限（天）" prop="sameStyleTimeLimit" />
+      <s-form-item component="el-input-number" :precision="0" :min="0" label="不同风格时限（天）" prop="differentStyleTimeLimit" />
+      <s-form-item component="el-input-number" :precision="0" :min="0" label="蓝色预警时限（天）" prop="blueWarningCycle" />
+      <s-form-item component="el-input-number" :precision="0" :min="0" label="黄色预警时限（天）" prop="yellowWarningCycle" />
+      <s-form-item component="el-input-number" :precision="0" :min="0" label="橙色预警时限（天）" prop="orangeWarningCycle" />
+      <s-form-item component="el-input-number" :precision="0" :min="0" label="红色预警时限（天）" prop="redWarningCycle" />
+      <s-form-item label="" component="s-group" :data="options" prop="dateType" />
+
+      <s-form-item>
+        <s-button @click="$emit('close')">取消</s-button>
+        <s-button type="primary" run="form.submit">确定</s-button>
+      </s-form-item>
+    </s-form>
+  </div>
+</template>
+<script>
+import { defineComponent, reactive, ref } from '@vue/composition-api'
+import _save from '@/api/1528-put-production-config-service-product-production-cycle-batch'
+import { Message } from 'element-ui'
+
+export default defineComponent({
+  props: {
+    data: {
+      type: Array,
+    },
+  },
+  setup({ data }, { emit, root }) {
+    let form = reactive({
+      baseCycle: '',
+      id: undefined,
+      sameStyleTimeLimit: '',
+      differentStyleTimeLimit: '',
+      blueWarningCycle: '',
+      yellowWarningCycle: '',
+      orangeWarningCycle: '',
+      redWarningCycle: '',
+      dateType: 1,
+    })
+
+    const save = (form) => {
+      const arr = data.map((v) => {
+        return {
+          ...form,
+          id: v.id,
+        }
+      })
+      return _save(arr).then(() => {
+        Message({
+          message: '保存成功！',
+          type: 'success',
+        })
+        emit('close')
+        root.$store.commit('table/update')
+      })
+    }
+
+    const options = ref([
+      { label: '自然日', value: 0 },
+      { label: '工作日', value: 1 },
+    ])
+
+    return {
+      save,
+      form,
+      options,
+    }
+  },
+})
+</script>
---
+++ src/views/produce/product-period/list.vue
@@ -0,0 +1,109 @@
+<template>
+  <div>
+    <s-dialog v-bind="dialog" @close="dialog.close" />
+    <s-simple-table v-model="table.checked" :data="table.data" :cols="table.cols">
+      <s-form slot="form" inline>
+        <s-form-item label="状态名称" prop="name" />
+        <!-- <s-form-item label="状态" prop="status" component="s-group" :data="options" /> -->
+        <s-form-item>
+          <s-button type="primary" run="form.search">查询</s-button>
+          <s-button run="form.reset">重置</s-button>
+        </s-form-item>
+      </s-form>
+      <div class="mb20" slot="top">
+        <s-button type="primary" :disabled="table.checked.length===0" @click="dialog.open({data:table.checked})">批量设置</s-button>
+      </div>
+    </s-simple-table>
+  </div>
+</template>
+<script>
+import { defineComponent, reactive } from '@vue/composition-api'
+import getTableData from '@/api/1526-get-production-config-service-product-production-cycle-search'
+import useDialog from '@/hooks/use-dialog'
+// import { Message } from 'element-ui'
+
+export default defineComponent({
+  setup() {
+    const dialog = useDialog({
+      uid: 'set-product-period',
+      title: '生产周期设置',
+      width: '500px',
+      component: require('./dialog/set'),
+    })
+
+    const table = reactive({
+      checked:[],
+      data: getTableData,
+      cols: [
+        {
+          type:'checkbox',
+          width:'44px'
+        },
+        {
+          showOverflowTooltip: true,
+          label: '服务产品名称',
+          prop: 'serviceProductName	',
+        },
+        {
+          label: '服务产品编码',
+          prop: 'serviceProductCode',
+        },
+        {
+          label: '服务产品分类',
+          prop:'serviceProductType',
+        },
+        {
+          label: '创建时间',
+          prop: 'createTime',
+        },
+        {
+          label: '基准生产周期（天）',
+          prop: 'baseCycle',
+        },
+        {
+          label: '同风格时限（天）',
+          prop: 'sameStyleTimeLimit',
+        },
+        {
+          label: '不同风格时限（天）',
+          prop: 'differentStyleTimeLimit',
+        },
+        {
+          label: '蓝色预警周期（天）',
+          prop: 'blueWarningCycle',
+        },
+        {
+          label: '黄色预警周期（天）',
+          prop: 'yellowWarningCycle',
+        },
+        {
+          label: '橙色预警周期（天）',
+          prop: 'orangeWarningCycle	',
+        },
+        {
+          label: '红色预警周期（天）',
+          prop: 'redWarningCycle',
+        },
+        {
+          label: '操作',
+          prop: ({ row }) => {
+            return [
+              <s-button
+                type="text"
+                onClick={() => dialog.open({ data: [row], isEdit: true })}
+              >
+                编辑
+              </s-button>,
+            ]
+          },
+        },
+      ],
+    })
+
+    return {
+      dialog,
+      table,
+    }
+  },
+})
+</script>
